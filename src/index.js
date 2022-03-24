888b60b71da0331504c2989a04bdbcd5dd49a0f4
Angelus123 <izerefaifelix@gmail.com>
2022-03-24 13:13:35 +0200

    add welcome

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
-// process.on("uncaughtException", (err) => {
-//   console.log(err.name, err.message);
-//   console.log("uncaught Exception * Shutdown");
-//   process.exit(1);
-// });
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("uncaught Exception * Shutdown");
+  process.exit(1);
+});
 dotenv.config({ path: "./config/config.env" });
 
 const port = process.env.PORT;
@@ -27,14 +27,18 @@ mongoose
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get('/', (req,res)=>{res.status(200).send({
+  status:200, 
+  message:'welcome to patiente registration',
+})})
 app.use("/api/v1/", userRouter);
 app.use("/api/v1/", blogRouter);
 app.use("/api/v1/", messageRouter);
 app.use(globalErrorHandling.globalErrorHandling);
 app.listen(port, () => console.log(`server is running at port ${port}`));
-// process.on("unhandledRejection", (err) => {
-//   console.log(err.name, err.message);
-//   console.log("Unhundled rejection * Shutdown");
-//   server.close(() => process.exit(1));
-// });
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhundled rejection * Shutdown");
+  server.close(() => process.exit(1));
+});
 export default app
